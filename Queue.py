da00db0df200b6ecf5df6c365ee8aa224794ca1d
Vaelastraszz <romain.lejeun@gmail.com>
2024-05-23 18:50:07 +0200

    change dequeueing and enqueing
    problem for the dequeing i dequeued the last element but it should be
    the first,
    plus enqueuing in was inserting at the beginning but should enqueue at
    the end

--- Queue.py
+++ Queue.py
@@ -11,13 +11,13 @@ class Queue:
         """Adds a new item to the rear of the queue.
 
         Args : item : object : the item to be added to the queue."""
-        self.items.insert(0, item)
+        self.items.append(item)
 
     def dequeue(self) -> object:
         """Removes and returns the front item from the queue.
 
         Returns : object : the front item of the queue."""
-        return self.items.pop()
+        return self.items.pop(0)
 
     def size(self) -> int:
         """Returns the number of items in the queue.
