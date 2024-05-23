6171c1c08c7e4774233d8f8d9f52aaf23288035d
Vaelastraszz <romain.lejeun@gmail.com>
2024-05-23 13:22:43 +0200

    added docstrings to the functions of queue.py
    Documentation of the functionnalities of queue class
    -isempty, dequeue, enqueue, size

--- Queue.py
+++ Queue.py
@@ -1,15 +1,27 @@
 class Queue:
     def __init__(self):
+        """Creates a new queue that is empty."""
         self.items = []
 
     def isEmpty(self):
+        """Tests to see whether the queue is empty."""
         return self.items == []
 
     def enqueue(self, item: object) -> None:
+        """Adds a new item to the rear of the queue.
+
+        Args : item : object : the item to be added to the queue."""
         self.items.insert(0, item)
 
     def dequeue(self) -> object:
+        """Removes and returns the front item from the queue.
+
+        Returns : object : the front item of the queue."""
         return self.items.pop()
 
     def size(self) -> int:
+        """Returns the number of items in the queue.
+        It needs no parameters and returns an integer.
+
+        Returns : int : the number of items in the queue."""
         return len(self.items)
